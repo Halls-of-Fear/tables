44047658e52443f210a2e2bef8c554f0af03b8c2
Halls-of-Fear <35615086+Halls-of-Fear@users.noreply.github.com>
2023-04-02 11:24:26 +0300

    button add to java after success

--- scripts/java1.js
+++ scripts/java1.js
@@ -1,22 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
   // Define the items array
-  const items = [
-    { material: 'Wood', type: 'Armor', price: 10 },
-    { material: 'Iron', type: 'Armor', price: 50 },
-    { material: 'Silver', type: 'Armor', price: 100 },
-    { material: 'Wood', type: 'Weapon', price: 5 },
-    { material: 'Iron', type: 'Weapon', price: 30 },
-    { material: 'Silver', type: 'Weapon', price: 80 },
+  let items = [
+    { material: 'Wood', type: 'Armor', price: 0 },
+    { material: 'Iron', type: 'Armor', price: 0 },
+    { material: 'Silver', type: 'Armor', price: 0 },
+    { material: 'Wood', type: 'Weapon', price: 0 },
+    { material: 'Iron', type: 'Weapon', price: 0 },
+    { material: 'Silver', type: 'Weapon', price: 0 },
   ];
 
-  // Loop through the items and populate the table cells with prices
-  items.forEach(item => {
-    const cell = document.getElementById(`${item.material.toLowerCase()}-${item.type.toLowerCase()}`);
-    cell.textContent = `${item.price} gold`;
-    cell.addEventListener('click', () => {
-      showItemDetails(item);
-    });
-  });
+  // Populate the table cells with prices
+  populateTable();
 
   // Define the showItemDetails function
   function showItemDetails(item) {
@@ -33,5 +27,40 @@ document.addEventListener('DOMContentLoaded', () => {
     const details = document.getElementById('details');
     details.innerHTML = '';
     details.removeEventListener('click', hideItemDetails);
+  }
+
+  // Define the refreshPrices function
+  function refreshPrices() {
+    // Generate random prices for each item
+    items.forEach(item => {
+      switch (item.material) {
+        case 'Wood':
+          item.price = Math.floor(Math.random() * 10) + 1;
+          break;
+        case 'Iron':
+          item.price = Math.floor(Math.random() * 40) + 11;
+          break;
+        case 'Silver':
+          item.price = Math.floor(Math.random() * 90) + 61;
+          break;
+      }
+    });
+
+    // Sort the items by price
+    items.sort((a, b) => a.price - b.price);
+
+    // Populate the table cells with the new prices
+    populateTable();
+  }
+
+  // Define the populateTable function
+  function populateTable() {
+    items.forEach(item => {
+      const cell = document.getElementById(`${item.material.toLowerCase()}-${item.type.toLowerCase()}`);
+      cell.textContent = `${item.price} gold`;
+      cell.addEventListener('click', () => {
+        showItemDetails(item);
+      });
+    });
   }
 });
